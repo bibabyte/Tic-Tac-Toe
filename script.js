9c10597df90fca9365d31511df8080b41f825861
Bhagavati Braun <bhagavati.braun@gmail.com>
2020-09-21 17:01:18 +0300

    Grid populates with borders

--- script.js
+++ script.js
@@ -1,29 +1,77 @@
 // === Minimize global code === \\
-// -- use factory functions and modules
+// use factory functions and modules
+//let container = document.querySelector('#container');
 
 // Gameboard - 3D array
 const gameboard = () => {
-	let box = document.querySelector('#container');
+	let container = document.querySelector('#container');
 	// Create a 2D array of divs
-	let rows = 3;
+	let boxes = 9;
 	
-	box.style.gridTemplateColumns.gridTemplateRows = `repeat(${rows}, 1fr)`;
+	/*container.style.gridTemplateColumns = `repeat(${boxes}, 1fr)`;
+	container.style.gridTemplateRows = `repeat(${boxes}, 1fr)`;*/
 	
 	// Dynamic sizing
-	/*if (window.innerHeight > window.innerWidth) {
-		box.style.width = 70vw;
+	if (window.innerHeight > window.innerWidth) {
+		container.style.width = '75vw';
 	} else {
-		box.style.width = 70vh;
-	}*/
-	for (rows; rows > 0; rows--) {
+		container.style.width = '75vh';
+	}
+	for (boxes; boxes > 0; boxes--) {
 		let board = document.createElement('div');
-		box.appendChild(board);	
-			let count = 0;
-			console.log(count);
-			count++;
+		board.classList.add('boxes');
+		board.textContent = boxes;
+		container.appendChild(board);	
+
 	}
 }
 gameboard();
+
+
+
+
+
+
+/*function setGrid() {
+	const gridContainer = document.createElement('div');
+	container.appendChild(gridContainer);
+	let gridWidth = 16;
+	let gridHeight= Math.floor(gridWidth * 0.75);
+	
+	// Set dynamic rows and columns (CSS grid)
+	gridContainer.className = 'boxes';
+	gridContainer.style.gridTemplateColumns = `repeat(${gridWidth}, 1fr)`;
+	gridContainer.style.gridTemplateRows = `repeat(${gridHeight}, 1fr)`;*/
+
+	/*// Set etch-a-sketch window size dependent on browser orientation
+	//------to learn: how to change if viewport changes after load-----
+	if(window.innerHeight > window.innerWidth){
+		gridContainer.style.width = `70vw`;
+		gridContainer.style.height = `52.5vw`;
+		
+	}
+	else {
+		gridContainer.style.width = `70vh`;
+		gridContainer.style.height = `52.5vh`;
+		
+	}
+*/
+	// Place divs in the etch-a-sketch
+	/*for(let i = gridWidth * gridHeight; i > 0; i--){
+		let etchGrid = document.createElement('div');
+		etchGrid.className = 'boxes';
+		
+	//	etchGrid.addEventListener("touchstart", touchStart);
+	
+		gridContainer.appendChild(etchGrid);
+	}
+	console.log('hi');
+} */
+
+
+
+
+
 /*function setGrid() {
 
 	
@@ -44,12 +92,12 @@ gameboard();
 
 //3D array
 
-let board = [
+/*let board = [
 	['x', '0', 'x'],
 	['0', 'x', '0'],
 	['x', '0', 'x']
 ]
-console.log(board);
+console.log(board);*/
 
 // Players
 const player = (name, symbol) => {
