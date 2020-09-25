1015c26d744b5af0fd56721a8363e243a103c1ab
Bhagavati Braun <bhagavati.braun@gmail.com>
2020-09-25 12:02:17 +0300

    Responsive board size
    Calculations nested in their own function: width();

--- script.js
+++ script.js
@@ -1,34 +1,47 @@
 // === Minimize global code === \\
-// use factory functions and modules
-//let container = document.querySelector('#container');
+// use factory functions and modules	
+
+let squares = [
+	/*['', '', ''],
+	['', '', ''],
+	['', '', '']*/
+]
+//console.log(squares);
 
 // Gameboard - 3D array
 const gameboard = () => {
 	let container = document.querySelector('#container');
 	// Create a 2D array of divs
 	let boxes = 9;
+	let cWidth = width();
+	//let squares = [];
 	// Dynamic sizing
-	if (window.innerHeight > window.innerWidth) {
-		container.style.width = '75vw';
-	} else {
-		container.style.width = '75vh';
-	}
+	container.style.width = cWidth;
+	container.style.height = cWidth;
 	// Populate boxes
 	for (boxes; boxes > 0; boxes--) {
 		let board = document.createElement('div');
 		board.classList.add('boxes');
+		board.style.width = cWidth/3;
+		board.style.height = cWidth/3;
 			board.textContent = boxes;
+		let i = 3;
+		/*for(i; i > 0; i++) {
+			squares[i].push(boxes);
+		}*/
 		board.addEventListener('click', turn);
 		container.appendChild(board);	
-	}
-}
-gameboard();
 
+	}
+		//console.log(squares);
+} 
+gameboard();
+ 
 
 function turn(e) {
 	if (player.id = 'x'){
 		e.target.textContent = 'X';
-	}
+	} 
 	e.target.textContent = 'O';
 }
 
@@ -39,12 +52,6 @@ function turn(e) {
 
 //3D array
 
-/*let board = [
-	['x', '0', 'x'],
-	['0', 'x', '0'],
-	['x', '0', 'x']
-]
-console.log(board);*/
 
 // Players
 const player = (name, id) => {
@@ -62,8 +69,17 @@ const player = (name, id) => {
 
 
 
+// Display width
+function width() {
+	if (window.innerHeight > window.innerWidth) {
+		return '75vw';
+	} 
+	return '75vh';
+}
 // ===== NOTES ==== \\
 /*
+	FIRST read through TOP instructions again
+	
 	create players with ID x, and o
 		each person inputs their name and the first chooses symbol
 			eventually have player choose any key and have (turn()) input that
